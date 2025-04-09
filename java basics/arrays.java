8fa8f1a17002d4ccc74c8c2ca566dc5833419d71
priyanshi pal <palpriyanshi712@gmail.com>
2025-04-09 05:42:04 +0000

    solve the question of buy and sell stock

--- java basics/arrays.java
+++ java basics/arrays.java
@@ -190,11 +190,31 @@ public class arrays {
         return trappedWater;
     }
 
+
+     //this code for buy and sell stock
+     public static int buyandsellstock(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for(int i=0; i<prices.length; i++){
+            if(buyprice < prices[i]){
+                int profit = prices[i] - buyprice;
+                maxprofit = Math.max(maxprofit, profit);
+            }else{
+                buyprice = prices[i];
+            }
+        }
+        return maxprofit;
+     }
+
     public static void main(String args[]) {
 
+         //this code for buy and sell stock
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(buyandsellstock(prices));
+
         //this code for find trapped water in bars
-        int height[] = {4, 2, 0, 6, 3, 2, 5};
-        System.out.println(trapperWater(height));
+        // int height[] = {4, 2, 0, 6, 3, 2, 5};
+        // System.out.println(trapperWater(height));
 
         // int num[] = {-2, -3, 4, -1, -2, 1, 5, -3};
         // kadanes(num);

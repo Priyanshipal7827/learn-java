458e1c9272f4828fbe589ad47e4fd51a90f4bb1c
priyanshi pal <palpriyanshi712@gmail.com>
2025-04-09 09:46:55 +0000

    optimize solution of bubble sort

---
+++ learn sorting/bubble.java
@@ -0,0 +1,24 @@
+public class bubble{
+    public static void bubblesort(int arr[]){
+        int n = arr.length;
+        boolean swapped;
+        for(int i=0; i<n; i++){
+            swapped = false;
+            for(int j=0; j<n-1-i; j++){
+                if(arr[j] < arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+       bubblesort(arr);
+    }
+}

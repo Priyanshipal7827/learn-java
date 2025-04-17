2b2be8ed45bf6fd409f6c44b2919d8537d92bf28
priyanshi pal <palpriyanshi712@gmail.com>
2025-04-17 05:43:08 +0000

    fixing error

--- learn sorting/insertionsort.java
+++ learn sorting/insertionsort.java
@@ -14,6 +14,9 @@ public class insertionsort {
     public static void main(String args[]) {
         int[] arr = { 5, 4, 1, 3, 2 };
         insertionSort(arr);
-        
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }

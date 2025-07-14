6aedf607e4bc6d1399a07e28143f1cedc982d1ee
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-14 07:04:02 +0000

    learn print statement

--- java basics/basics.java
+++ java basics/basics.java
@@ -1,5 +1,16 @@
 public class basics {
     public static void main(String arg[]){
-        System.out.println("hello world");
+        // print statement
+        System.out.println("Hello world");
+        System.out.println("Prianshi pal");
+        System.out.println(12345);
+        // print star pattern
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+        // varibles
+
     }
 }

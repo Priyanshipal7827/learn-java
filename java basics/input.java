eb67a928136628acbdbfbd583c07e41cffcb1b24
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-14 16:43:43 +0000

    learn type conversion and type casting

--- java basics/input.java
+++ java basics/input.java
@@ -3,11 +3,24 @@ import java.util.*;
 public class input {
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
-        // sum of a , b
-        int a = sc.nextInt("Enter  First Num :");
-        int b = sc.nextInt("Enter Second Num :");
-        int sum = a + b;
-        System.out.println(sum);
+        // ****************sum of a , b***************
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println(sum);
+        // ****************product of a , b*****************
+        // int prod = a * b;
+        // System.out.println(prod);
+        // ************find the area of a circle*********
+        // Float rad = sc.nextFloat();
+      
+        // Float area = 3.14f * rad * rad;
+        // System.out.println(area);
         sc.close();
+        // ****************type conversion*****************
+        // int a = 23;
+        // long b = a; 
+     
+        // System.out.println(b);
     }
 }

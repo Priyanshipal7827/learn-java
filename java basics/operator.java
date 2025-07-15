e74ca4b8df19db944120bd5bfc8f0fddd01ee36b
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-15 11:24:33 +0000

    unary operators

--- java basics/operator.java
+++ java basics/operator.java
@@ -2,6 +2,7 @@ public class operator {
     public static void main(String arg[]){
         // ********** learn operators*********
         // Arithamatic operator 
+        // binary arithmatic operator
         int a = 10;
         int b = 5;
         int add = a + b;
@@ -14,6 +15,8 @@ public class operator {
         System.out.println(mul);
         System.out.println(div);
         System.out.println(mod);
+        // unarry arithmatic operattor
+        
 
     }
 }

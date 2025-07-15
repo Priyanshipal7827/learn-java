b6312e3ec9db676d766b7efb7b34954cd359f4e3
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-15 17:03:03 +0000

    learn if else statement

--- java basics/statement.java
+++ java basics/statement.java
@@ -1,3 +1,16 @@
 public class statement {
-    
+    public static void main(String arg[]){
+        // *************conditional statement************
+        // if else statement
+        int age = 14;
+        if (age>=18) {
+            System.out.println("Adult : you can drive, vote.");
+        } 
+        if(age>13 && age<18){
+            System.out.println("Teenager");
+        }
+        else {
+            System.out.println("Not adult!");
+        }
+    }
 }

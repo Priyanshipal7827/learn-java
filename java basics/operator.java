90d63aefec19edc186bf676b71f88c09c6efe213
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-15 11:49:59 +0000

    relational operators

--- java basics/operator.java
+++ java basics/operator.java
@@ -1,22 +1,32 @@
 public class operator {
-    public static void main(String arg[]){
+    public static void main(String arg[]) {
         // ********** learn operators*********
-        // Arithamatic operator 
+        // Arithamatic operator
         // binary arithmatic operator
         int a = 10;
         int b = 5;
-        int add = a + b;
-        int sub = a - b;
-        int mul = a * b;
-        int div = a / b;
-        int mod = a % b;
-        System.out.println(add);
-        System.out.println(sub);
-        System.out.println(mul);
-        System.out.println(div);
-        System.out.println(mod);
+        // int add = a + b;
+        // int sub = a - b;
+        // int mul = a * b;
+        // int div = a / b;
+        // int mod = a % b;
+        // System.out.println(add);
+        // System.out.println(sub);
+        // System.out.println(mul);
+        // System.out.println(div);
+        // System.out.println(mod);
         // unarry arithmatic operattor
-        
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(b);
+        // *****************relational operators********************
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+
 
     }
 }

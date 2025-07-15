5789be8c7c2574a593f6cbdff493e3eddf796018
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-15 11:21:37 +0000

    learn arithmatic operator

--- java basics/operator.java
+++ java basics/operator.java
@@ -1,3 +1,19 @@
 public class operator {
-    
+    public static void main(String arg[]){
+        // ********** learn operators*********
+        // Arithamatic operator 
+        int a = 10;
+        int b = 5;
+        int add = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a % b;
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+
+    }
 }

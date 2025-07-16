cfc10465f659d7e660e71f8976c3abf449ca968b
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-16 11:58:35 +0000

    eslse if statement

--- java basics/statement.java
+++ java basics/statement.java
@@ -3,6 +3,7 @@ public class statement {
     public static void main(String arg[]){
         // *************conditional statement************
         // if else statement
+      
         int age = 14;
         if (age>=18) {
             System.out.println("Adult : you can drive, vote.");
@@ -22,6 +23,7 @@ public class statement {
             System.out.println("B is largest of two number");
         }
         // print even and odd number useing input
+        System.out.println("Enter your number :  ");
         Scanner sc = new Scanner(System.in);
         int number =  sc.nextInt();
         if(number % 2 == 0){
@@ -29,6 +31,17 @@ public class statement {
         } else{
             System.out.println("Number is odd : " +  number);
         }
+        // learn else if statement 
+        System.out.println("Enter your age :  ");
+        int age1 = sc.nextInt();
+        if(age1 >= 18){
+            System.out.println("Adult");
+        } else if(age1 >= 113 && age1 <= 18){
+            System.out.println("Teenager");
+        } else{
+            System.out.println("Child");
+        }
+
         sc.close();
     }
 }

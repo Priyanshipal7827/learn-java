9989d639ba68f837bbdf9019e9197707ff275cf7
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-17 12:18:21 +0000

    basic code of while loop

--- java basics/loops.java
+++ java basics/loops.java
@@ -1,5 +1,10 @@
 public class loops {
     public static void main(String arg[]){
         //learn loops 
+        int count = 0;
+    while(count <= 5){
+        System.out.println("hello priyanshi!");
+        count++;
+    }
     }
 }

90cde90f4d7d9296e19eba3d310012511a3ddb64
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-17 12:37:36 +0000

    print satemnt

--- java basics/loops.java
+++ java basics/loops.java
@@ -1,10 +1,16 @@
 public class loops {
     public static void main(String arg[]){
         //learn loops 
-        int count = 0;
-    while(count <= 5){
-        System.out.println("hello priyanshi!");
+        int count = 1;
+    while(count < 5){
+        System.out.println( count + " hello priyanshi!" );
         count++;
+    }
+    // print 1 to 10 number using while loop 
+    int num = 1;
+    while(num<=10){
+        System.out.println(num);
+        num++;
     }
     }
 }

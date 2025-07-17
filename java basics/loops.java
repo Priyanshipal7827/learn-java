f30df8d108807ec3306c0b7dc7552b65a8d33e26
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-17 17:28:24 +0000

    sum of n numbers

--- java basics/loops.java
+++ java basics/loops.java
@@ -1,27 +1,41 @@
 import java.util.*;
+
 public class loops {
-    public static void main(String arg[]){
-        //learn loops 
+    public static void main(String arg[]) {
+        // learn loops
         int count = 1;
-    while(count < 5){
-        System.out.println( count + " hello priyanshi!" );
-        count++;
-    }
-    // print 1 to 10 number using while loop 
-    int num = 1;
-    while(num<=10){
-        System.out.println(num);
-        num++;
-    }
-    // print  number  1 to n 
-    Scanner sc = new Scanner(System.in);
-    int i = 1;
-    int n = sc.nextInt();
-    while(i <= n){
-        System.out.println(i);
-        i++;
-    }
-    sc.close();
+        while (count < 5) {
+            System.out.println(count + " hello priyanshi!");
+            count++;
+        }
+        // print 1 to 10 number using while loop
+        int num = 1;
+        while (num <= 10) {
+            System.out.println(num);
+            num++;
+        }
+        // print number 1 to n
+        // System.out.println("Print n numbers ");
+        Scanner sc = new Scanner(System.in);
+        // int i = 1;
+        // int n = sc.nextInt();
+        // while(i <= n){
+        // System.out.println(i);
+        // i++;
+        // }
+
+        // print sum of natural numbers
+        System.out.println("Enter value of n1 : ");
+        int j = 1;
+        int n1 = sc.nextInt();
+        int sum = 0;
+        while (j <= n1) {
+            sum += j;
+            j++;
+        }
+        System.out.println("Sum is : " + sum);
+
+        sc.close();
 
     }
 }

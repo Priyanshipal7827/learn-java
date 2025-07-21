7ad4e1a41e95116ff0f1d3f291ca347048ef68ab
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-21 17:05:45 +0000

    question 2 of operator

--- java basics/question.java
+++ java basics/question.java
@@ -24,13 +24,25 @@ public class question {
         System.out.println("Bill of : " + newtotal);
 
         // ***************quetion based on operators ***********
-
+        // question 1
         int x = 2, y = 5;
         int exp1 = (x * y / x);
         int exp2 = (x * (y / x));
-        System.out.println(exp1 + " , ");
+        System.out.println(exp1);
         System.out.println(exp2);
 
-        sc.close();
+        // question 2
+        int x1 = 200, y1 = 50, z1 = 100;
+        if (x1 > y1 && y1 > z1) {
+            System.out.println("Hello");
+        }
+        if (z1 > y1 && z1 < x1) {
+            System.out.println("Java");
+        }
+        if ((y1 + 200) < x1 && (y1 + 150) < z1) {
+            System.out.println("Hello Java");
+
+            sc.close();
+        }
     }
 }

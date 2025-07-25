fadaf3d91c01ab9255b90c91abfdee1dcc609c96
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-25 17:10:57 +0000

    variable question

--- javaBasic/assignmentQuestion/variable.java
+++ javaBasic/assignmentQuestion/variable.java
@@ -6,15 +6,18 @@ public class variable {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int sum = a + b ;
+        int sum = a + b;
         System.out.println("Sum of two numbers : " + sum);
 
-          // ***********Question 1 ***************
+        // ***********Question 1 ***************
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int z = sc.nextInt();
-        int avg = (x+y+z)/3;
+        System.out.print("Enter num 1 : ");
+        float x = sc.nextFloat();
+        System.out.print("Enter num 2 : ");
+        float y = sc.nextFloat();
+        System.out.print("Enter num 3 : ");
+        float z = sc.nextFloat();
+        float avg = (x + y + z) / 3;
         System.out.println("Average of three number : " + avg);
         sc.close();
     }

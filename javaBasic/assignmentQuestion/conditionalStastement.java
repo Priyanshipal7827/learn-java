06826b2d8b737f4eb96bf2acb14e071004b47550
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-26 17:37:57 +0000

    conditional statement question 3

--- javaBasic/assignmentQuestion/conditionalStastement.java
+++ javaBasic/assignmentQuestion/conditionalStastement.java
@@ -18,15 +18,44 @@ public class conditionalStastement {
             System.out.println("Number is zero : " + num);
         }
 
-
-
         // *************** question 2 **********
         System.out.print("Enter your temperature : ");
         double temp = sc.nextDouble();
-        if(temp > 100){
+        if (temp > 100) {
             System.out.println("You have a fever because your temp is : " + temp);
-        } else{
+        } else {
             System.out.println("You don't have a fever because your temp is : " + temp);
+        }
+
+        // ************ question 3 ****************
+        System.out.print("Enter the week number : ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Today is Sunday.");
+                break;
+            case 2:
+                System.out.println("Today is Monday.");
+                break;
+            case 3:
+                System.out.println("Today is Tuesday.");
+                break;
+            case 4:
+                System.out.println("Today is Wednesday.");
+                break;
+            case 5:
+                System.out.println("Today is thursday.");
+                break;
+            case 6:
+                System.out.println("Today is friday.");
+                break;
+            case 7:
+                System.out.println("Today is saturday.");
+                break;
+            default:
+                System.out.println("Something went wrong!");
+
         }
 
         sc.close();

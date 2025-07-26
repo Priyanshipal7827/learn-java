da58a405957ff9af78d80c039560910f20cd99d6
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-26 17:22:19 +0000

    conditional statement question 1

--- javaBasic/assignmentQuestion/conditionalStastement.java
+++ javaBasic/assignmentQuestion/conditionalStastement.java
@@ -1,5 +1,24 @@
 package assignmentQuestion;
 
+import java.util.*;
+
 public class conditionalStastement {
-    
+
+    // ************* question 1 *************
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println("Number is positive : " + num);
+        } else if (num < 0) {
+            System.out.println("Number is negative : " + num);
+        } else {
+            System.out.println("Number is zero : " + num);
+        }
+
+        sc.close();
+    }
+
 }

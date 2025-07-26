3a1a157819458ed5add7a053ac6908720729f8df
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-26 13:12:07 +0000

    variable question

--- javaBasic/assignmentQuestion/variable.java
+++ javaBasic/assignmentQuestion/variable.java
@@ -9,6 +9,8 @@ public class variable {
         int sum = a + b;
         System.out.println("Sum of two numbers : " + sum);
 
+
+
         // ***********Question 1 ***************
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter num 1 : ");
@@ -19,6 +21,51 @@ public class variable {
         float z = sc.nextFloat();
         float avg = (x + y + z) / 3;
         System.out.println("Average of three number : " + avg);
+
+
+
+
+
+        // ****************question 2 *********
+        System.out.print("Enter value of side : ");
+        int side = sc.nextInt();
+        int area = side*side;
+        System.out.println("Area of side : " + area);
+
+
+
+
+        // *******************question 3 ************
+        System.out.print("Enter the price of pencil : ");
+        int pencil = sc.nextInt();
+        System.out.print("Enter the price of pen : ");
+        int pen = sc.nextInt();
+        System.out.print("Enter the price of eraser : ");
+        int eraser = sc.nextInt();
+        int total = (pen+pencil+eraser);
+        System.out.println("Total is : " + total);
+        float newTotal = (total * 0.18f) + total;
+        System.out.println("GST of items : " + newTotal);
+
+
+
+        // *************** question 4 **********
+
+        byte B = 4;
+        char c = 'a';
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f;
+        double d = 99.9954;
+        double result = (f*b) + (i/c) - (d*s);
+        System.out.println(result);
+        
+
+        
+
+
+
+
         sc.close();
     }
 }

bfb15d4f7273cfb353c0b72df9102b6758073424
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-26 16:46:40 +0000

    operator question 2

--- javaBasic/assignmentQuestion/operator.java
+++ javaBasic/assignmentQuestion/operator.java
@@ -10,8 +10,30 @@ public class operator {
         System.out.println(exp1);
         System.out.println(exp2);
 
-
         // ************* question 2 **********
-        
+        int x1 = 200, y1 = 50, z1 = 100;
+        if (x1 > y1 && y1 > z1) {
+            System.out.println("Hello");
+        }
+        if (z1 > y1 && z1 < x1) {
+            System.out.println("Java");
+        }
+        if ((y1 + 200) < x1 && (y1 + 150) < z1) {
+            System.out.println("Hello Java");
+
+        }
+
+        // **************** question 3 **************
+
+        int x2, y2, z2;
+        x2 = y2 = z2 = 2;
+        x2 += y2;
+        y2 -= z2;
+        z2 /= (x2 + y2);
+        System.out.println(x2 + " " + y2 + " " + z2);
+
+
+
+        //  **************** question 4
     }
 }

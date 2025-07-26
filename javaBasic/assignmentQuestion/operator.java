76dbea8649aea2ebad92c9ffd13380dbb281835b
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-26 16:31:09 +0000

    operator assignment question

--- javaBasic/assignmentQuestion/operator.java
+++ javaBasic/assignmentQuestion/operator.java
@@ -1,10 +1,17 @@
 package assignmentQuestion;
 
-
-
-public class operator{
+public class operator {
     public static void main(String[] args) {
-      
+        // ************ question 1 *********
 
+        int x = 2, y = 5;
+        int exp1 = (x * y / x);
+        int exp2 = (x * (y / x));
+        System.out.println(exp1);
+        System.out.println(exp2);
+
+
+        // ************* question 2 **********
+        
     }
 }

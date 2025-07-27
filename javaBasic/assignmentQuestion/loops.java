74ec7ac876f9f00cebff3bd45ad4c40fe20fd8a7
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-27 16:48:46 +0000

    delete a file

--- javaBasic/assignmentQuestion/loops.java
+++ javaBasic/assignmentQuestion/loops.java
@@ -2,7 +2,16 @@ package assignmentQuestion;
 
 public class loops {
     public static void main(String[] args) {
+        // ************ question 1 ****************
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello " + i);
+            // i += 2;
+        }
+
+
+        // ****************** question 2 ************
+
         
     }
-    
+
 }

33a075f36f2040f953069204b6105aed4d28c25e
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-27 17:20:47 +0000

    correction in question 4

--- javaBasic/assignmentQuestion/loops.java
+++ javaBasic/assignmentQuestion/loops.java
@@ -1,21 +1,24 @@
 package assignmentQuestion;
+import java.util.*;
+
 
 public class loops {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         // learn loops
         // while loop
 
-        int n = 1;
-        while (n <= 5) {
-            System.out.println("hello priyanshi! " + n);
-            n++;
-        }
+        // int n = 1;
+        // while (n <= 5) {
+        // System.out.println("hello priyanshi! " + n);
+        // n++;
+        // }
 
         // do while loop
         int newNum = 1;
         do {
-            System.out.println(newNum);
+            System.out.print(newNum);
             newNum++;
         } while (newNum <= 5);
 
@@ -31,10 +34,19 @@ public class loops {
             // i += 2;
         }
 
+        // ***************** question 4 ************
+        System.out.print("Enter the value : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " * " + n + " = " + i*n );
+        }
+
         // ****************** question 5************
         for (int i = 0; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+
+        sc.close();
 
     }
 

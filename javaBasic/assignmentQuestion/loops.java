0c1f219f1397b3a722107f1bcf1a11b8001823fe
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-27 16:59:47 +0000

    learn while loop

--- javaBasic/assignmentQuestion/loops.java
+++ javaBasic/assignmentQuestion/loops.java
@@ -2,6 +2,15 @@ package assignmentQuestion;
 
 public class loops {
     public static void main(String[] args) {
+
+        // learn loops 
+        // while loop
+
+        int n = 1;
+        while(n<=5){
+            System.out.println("hello priyanshi! " + n);
+            n++;
+        }
         // ************ question 1 ****************
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello " + i);

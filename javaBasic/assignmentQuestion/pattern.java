7a89f2f26bfa83aa93d2f508dc10353d3773b8c0
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-29 07:59:00 +0000

    print inverted star pattern

--- javaBasic/assignmentQuestion/pattern.java
+++ javaBasic/assignmentQuestion/pattern.java
@@ -11,5 +11,16 @@ public class pattern {
             }
             System.out.println(" ");
         }
+
+
+        // inverted star pattern 
+        System.out.println("print inverted star pattern.");
+        int n = 5;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=(n-i+1);j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
     }
 }

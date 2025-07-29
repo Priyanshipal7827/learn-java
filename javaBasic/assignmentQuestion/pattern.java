8af4b19a6476f74bf137264c61f90e405a3d99a8
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-29 07:50:04 +0000

    print star pattern using nested for loop

--- javaBasic/assignmentQuestion/pattern.java
+++ javaBasic/assignmentQuestion/pattern.java
@@ -2,6 +2,14 @@ package assignmentQuestion;
 
 public class pattern {
     public static void main(String[] args) {
-        
+        // learn nested loops 
+
+        // print star pattern
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
     }
 }

b32abf9b89f8d1a5d9e49449051bb3da77a45591
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-29 07:10:13 +0000

    find the pair of numbers

--- javaBasic/assignmentQuestion/loops.java
+++ javaBasic/assignmentQuestion/loops.java
@@ -1,6 +1,6 @@
 package assignmentQuestion;
-import java.util.*;
 
+import java.util.*;
 
 public class loops {
     public static void main(String[] args) {
@@ -18,56 +18,58 @@ public class loops {
         // do while loop
         // int newNum = 1;
         // do {
-        //     System.out.print(newNum);
-        //     newNum++;
+        // System.out.print(newNum);
+        // newNum++;
         // } while (newNum <= 5);
 
         // for loop
 
         // for (int k = 1; k <= 5; k++) {
-        //     System.out.println(k);
+        // System.out.println(k);
         // }
 
         // // ************ question 1 ****************
         // for (int i = 0; i < 5; i++) {
-        //     System.out.println("Hello " + i);
-        //     // i += 2;
+        // System.out.println("Hello " + i);
+        // // i += 2;
         // }
-
-
 
         // ****************** question 2 ************
-        int number = sc.nextInt();
+        // int number = sc.nextInt();
         // int sum = 0;
         // for(int i = 1; i<=number; i++){
-        //      sum += i ;
+        // sum += i ;
         // }
-        //     System.out.println(sum);
+        // System.out.println(sum);
 
-            // ******************** question 3 **************
-            // print table of n number
+        // ******************** question 3 **************
+        // print table of n number
 
-            for(int i = 1; i <= 10; i++){
-                System.out.println(number*i);
-            }
-
-
-
+        // for(int i = 1; i <= 10; i++){
+        // System.out.println(number*i);
+        // }
 
         // ***************** question 4 ************
         // System.out.print("Enter the value : ");
         // int n = sc.nextInt();
         // for (int i = 1; i <= n; i++) {
-        //     System.out.println(i + " * " + n + " = " + i*n );
+        // System.out.println(i + " * " + n + " = " + i*n );
         // }
-
-
-
 
         // ****************** question 5************
         // for (int i = 0; i <= 5; i++) {
-        //     System.out.println("i = " + i);
+        // System.out.println("i = " + i);
         // }
+
+        // ************ question no 6 **************
+
+        int i = 0;
+        int j = 10;
+        while (i < j) {
+            System.out.println(i+ " " + j);
+            i++;
+            j--;
+        }
 
         sc.close();
 

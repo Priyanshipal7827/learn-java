edc8f0522a078548045c27e0544b1008c5bae4fe
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-30 03:14:41 +0000

    crete a fuction for print hello world

--- javaBasic/methods.java
+++ javaBasic/methods.java
@@ -1,5 +1,20 @@
 public class methods {
+
+
+     // functions are block of code that perform a specific task
+        // syntax retuenType name (){
+        // to do work
+        // return statement;
+        // }
+
+    public static void printHello(){
+        System.out.println("Hello world!");
+    }
+
+    // function for calculate sum 
+    
     public static void main(String[] args) {
-        
+       
+         printHello();  
     }
 }

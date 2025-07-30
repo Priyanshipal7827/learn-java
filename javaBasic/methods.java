6bd5bd40cf52c2f7015cf26d2bf2cb6013b5fb2a
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-30 03:27:45 +0000

    methods with parameters

--- javaBasic/methods.java
+++ javaBasic/methods.java
@@ -1,20 +1,30 @@
+import java.util.*;
+
 public class methods {
 
+    // functions are block of code that perform a specific task
+    // syntax retuenType name (){
+    // to do work
+    // return statement;
+    // }
 
-     // functions are block of code that perform a specific task
-        // syntax retuenType name (){
-        // to do work
-        // return statement;
-        // }
-
-    public static void printHello(){
+    public static void printHello() {
         System.out.println("Hello world!");
     }
 
-    // function for calculate sum 
-    
+    // function for calculate sum
+    public static int calculateSum(int a, int b) { // parameters an formal parameters
+        int sum = a + b;
+        return sum;
+    }
+
     public static void main(String[] args) {
-       
-         printHello();  
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = calculateSum(a, b); // arguments or actual parameters
+        System.out.println("sum is : " + sum);
+        printHello(); // function call
+        sc.close();
     }
 }

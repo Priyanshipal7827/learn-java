dd4d10df455f4e62bcda382213eb1a22f549d6a6
priyanshi pal <palpriyanshi712@gmail.com>
2025-07-30 03:56:52 +0000

    calculate the sum and swap numbers

--- javaBasic/methods.java
+++ javaBasic/methods.java
@@ -14,17 +14,31 @@ public class methods {
 
     // function for calculate sum
     public static int calculateSum(int a, int b) { // parameters an formal parameters
-        int sum = a + b;
-        return sum;
-    }
-
-    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = calculateSum(a, b); // arguments or actual parameters
-        System.out.println("sum is : " + sum);
-        printHello(); // function call
-        sc.close();
+        int sum = a + b;
+         sc.close();
+        return sum;
+    }
+
+    // swap values
+
+    public static void swap(int num1, int num2) {
+        int num1 = 4;
+        int num2 = 2;
+        // swap
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println(" num1 = : " + num1);
+        System.out.println(" num2 = : " + num2);
+
+    }
+
+    public static void main(String[] args) {
+        calculateSum(a, b);
+        printHello();
+       
     }
 }
